26f01f1b677c1338719f50b36cbaba570e23f8de
TheBestMario <mariogeorgiev02@gmail.com>
2025-01-09 14:57:20 +0000

    trying to get scene change to work

--- src/main/java/classes/LoginView.java
+++ src/main/java/classes/LoginView.java
@@ -1,30 +1,11 @@
 package classes;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
 
-import java.awt.*;
-import java.io.IOException;
+public class LoginView {
 
-public class LoginView extends Application {
     @FXML
-    private TextField username;
-    private PasswordField password;
-    public LoginView() throws IOException {
-        fxmlLoader = new FXMLLoader(Application.class.getResource("LoginView.fxml"));
-        System.out.println(fxmlLoader.getLocation());
-        fxmlLoader.setController(this);
-        scene = new Scene(fxmlLoader.load(), 600, 400);
-
-        getStage().setScene(scene);
-        getStage().show();
-    }
-    @FXML
-    public void onSubmitClick() {
-        System.out.println("submit clicked");
-
+    public void onSubmitClicked(){
+        System.out.println("Submit Clicked");
     }
 }
-
