cbefd0714cd29f945e1cd4137ef996343b9008fd
TheBestMario <mariogeorgiev02@gmail.com>
2025-01-09 14:59:55 +0000

    testing1

--- src/main/java/classes/Application.java
+++ src/main/java/classes/Application.java
@@ -1,3 +1,4 @@
+// src/main/java/classes/Application.java
 package classes;
 
 import javafx.fxml.FXMLLoader;
@@ -7,27 +8,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
-    private static Stage stage;
-    protected static FXMLLoader fxmlLoader;
-    protected Scene scene;
+    private static SceneController sceneController;
 
     @Override
     public void start(Stage stage) throws IOException {
-
-        this.stage = stage;
-
-        fxmlLoader = new FXMLLoader(Application.class.getResource("StartView.fxml"));
-        scene = new Scene(fxmlLoader.load(), 320, 240);
-
-        this.stage.setTitle("Hello!");
-        this.stage.setScene(scene);
-        this.stage.show();
+        sceneController = new SceneController(stage);
+        sceneController.switchToScene("StartView.fxml");
     }
-    public static Stage getStage(){
-        return stage;
-    }
-    public static void setFxmlLoader(FXMLLoader fxmlLoader){
-        Application.fxmlLoader = fxmlLoader;
+
+    public static SceneController getSceneController() {
+        return sceneController;
     }
 
     public static void main(String[] args) {
--- src/main/java/classes/LoginView.java
+++ src/main/java/classes/LoginView.java
@@ -2,10 +2,13 @@ package classes;
 
 import javafx.fxml.FXML;
 
+import java.io.IOException;
+
 public class LoginView {
 
     @FXML
-    public void onSubmitClicked(){
+    public void onSubmitClicked() throws IOException {
         System.out.println("Submit Clicked");
+        Application.getSceneController().switchToScene("StartView.fxml");
     }
 }
